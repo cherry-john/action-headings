d414dd92022cb59d75436f8c643c3f318584366e
John Cherry <github@johncherry.me>
2022-03-31 10:22:12 +0100

    add Branching code

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const core = require('@actions/core');
 const { updateAllHeadings } = require('./src/headings');
+const { createBranch } = require('./src/githubBranch');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     updateAllHeadings();
+    createBranch(String(new Date().getTime()));
   } catch (error) {
     core.setFailed(error.message);
   }
---
+++ src/githubBranch.js
@@ -0,0 +1,20 @@
+const { getOctokit, context } = require('@actions/github');
+const crypto = require('crypto');
+
+async function newBranch(branchName) {
+    const toolkit = getOctokit(process.env.GITHUB_TOKEN);
+
+    let ref = "refs/heads/"+branchName;
+    let hasher = crypto.createHash('sha1');
+    hasher.update(ref);
+    let hash = hasher.digest('hex');
+
+    toolkit.rest.git.createRef({
+        owner: "",
+        repo: context.repo,
+        ref: ref,
+        sha: hash        
+    });
+}
+
+module.exports = {newBranch}
