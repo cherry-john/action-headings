e50f06a568ab5dda54dae7a25c5adbc800be4a7b
John Cherry <github@johncherry.me>
2022-04-25 21:27:27 +0100

    update getValidHeading

--- src/headings.js
+++ src/headings.js
@@ -5,20 +5,28 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 /**
- * Returns the next valid heading level, based on the given currentHeading.
- * eg if heading="h3" and currentHeading="h1", this returns "h2", as that has been missed 
- * @param {String} heading tagName to verify
- * @param {String} currentHeading current highest relevant heading tagName
- * @returns {String}
- */
-const getValidHeading = (heading, currentHeading) => {
+     * Returns the next valid heading level, based on the given currentHeading.
+     * eg if heading="h3" and currentHeading="h1", this returns "h2", as that has been missed 
+     * @param {String} heading tagName to verify
+     * @param {String} currentHeading current highest relevant heading tagName
+     * @param {String} previousCurrentHeading what was the previous heading before it was changed?
+     * @returns {String}
+     */
+ const getValidHeading = (heading, currentHeading, previousCurrentHeading) => {
     const headingLevel = parseInt(heading.charAt(1));
+    const previousLevel = parseInt(previousCurrentHeading.charAt(1));
     const currentHeadingLevel = parseInt(currentHeading.charAt(1));
-
-    if (headingLevel <= currentHeadingLevel || headingLevel == currentHeadingLevel + 1){
-        return heading;
+    if (previousLevel == headingLevel) {
+        return currentHeading;
     }
-            
+    if ((headingLevel <= currentHeadingLevel || headingLevel == currentHeadingLevel + 1)){
+        if (previousLevel != headingLevel) {
+            return heading;
+        } else {
+            return currentHeading;
+        }
+        
+    }
     return "h" + (currentHeadingLevel + 1).toString();
 }
 
@@ -33,12 +41,14 @@ const updateHeadings = (filename) => {
         const dom = new JSDOM(data); //create JS DOM element to query
         
         let currentHeading = "h0";
+        let previousCurrentHeading = "h0";//store what the previous level was before changing
         let tags = dom.window.document.querySelectorAll("h1, h2, h3, h4, h5, h6")
         for (const tag in tags) {
             if (Object.hasOwnProperty.call(tags, tag)) {
                 const element = tags[tag];
                 //create replacement element
-                var new_element = dom.window.document.createElement(getValidHeading(element.tagName, currentHeading));
+                var new_element = dom.window.document.createElement(getValidHeading(element.tagName, currentHeading, previousCurrentHeading));
+                previousCurrentHeading = elem.tagName;
                 //add attributes
                 for(var i = 0; i < element.attributes.length; ++i){
                     let attribute = element.attributes.item(i);
